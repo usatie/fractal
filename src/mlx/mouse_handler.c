43a0f0869f6a5125c061d062ba4b9e1d55717cb4
Shun Usami <usatie@gmail.com>
2022-09-18 18:01:16 +0900

    :recycle: Add comments to mouse_handler

--- src/mlx/mouse_handler.c
+++ src/mlx/mouse_handler.c
@@ -6,7 +6,7 @@
 /*   By: susami <susami@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 10:36:22 by susami            #+#    #+#             */
-/*   Updated: 2022/09/18 14:25:48 by susami           ###   ########.fr       */
+/*   Updated: 2022/09/18 17:59:51 by susami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,60 @@
 #include "fractol.h"
 #include "fractol_ctx.h"
 
+/*
+
+step:
+   each -/|'s size is step
+             
+o:
+   The coordinate of the left top point of the window.
+
+                (0,0)                        (x, 0)
+   this point -> o-----------------------------*-------------------.
+	             |                                                 |
+	             |                                                 |
+	             |                                                 |
+	             |                                                 |
+	             |                     window                      |
+	             |                                                 |
+	      (0, y) *                             *                   |
+	             |                           mouse (x, y)          |
+	             |                                                 |
+	             .-------------------------------------------------.
+
+
+before zoom :
+	mouse_before = o + step * (x, -y)
+
+zoom :
+	step is updated.
+
+after zooom : 
+	o is updated.
+	o = mouse_before - step * (x, -y)
+
+* Note that y axis is upside down.
+
+*/
+
+#define MOUSE_WHEEL_UP 4
+#define MOUSE_WHEEL_DOWN 5
+
+// zoom in/out can be done by updating `step_n` and `o`
 int	mouse_handler(int button, int x, int y, t_ctx *ctx)
 {
 	double	next_step;
 	double	prev_step;
 
 	prev_step = step(ctx->step_n);
-	if (button == 4)
+	if (button == MOUSE_WHEEL_UP)
 		ctx->step_n--;
-	else if (button == 5)
+	else if (button == MOUSE_WHEEL_DOWN)
 		ctx->step_n++;
 	else
 		return (0);
 	next_step = step(ctx->step_n);
-	ctx->o.x += x * (prev_step - next_step);
-	ctx->o.y -= y * (prev_step - next_step);
+	ctx->o.x += (double)x * (prev_step - next_step);
+	ctx->o.y -= (double)y * (prev_step - next_step);
 	return (0);
 }
