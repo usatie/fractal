482f473cc17d7cb331f091c0db3187306a6bd199
Shun Usami <usatie@gmail.com>
2022-09-26 15:08:21 +0900

    :key_handler, simple keys

--- src/hooks/key_handler.c
+++ src/hooks/key_handler.c
@@ -6,17 +6,33 @@
 /*   By: susami <susami@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 19:25:09 by susami            #+#    #+#             */
-/*   Updated: 2022/09/23 19:30:19 by susami           ###   ########.fr       */
+/*   Updated: 2022/09/26 15:07:28 by susami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol_core.h"
 #include "keymap.h"
+#include "draw.h"
 #include "hooks.h"
 
 int	key_handler(int keycode, t_env *e)
 {
+	t_fractal	*f;
+
+	f = e->fractal;
 	if (keycode == MK_ESCAPE)
 		close_window(e);
+	else if (keycode == MK_BRACKETLEFT && f->max_loop > 10)
+		f->max_loop -= 10;
+	else if (keycode == MK_BRACKETRIGHT && f->max_loop)
+		f->max_loop += 10;
+	else if (keycode == MK_LEFT)
+		f->center.x -= pixel_width(f->zoom_level) * 10;
+	else if (keycode == MK_UP)
+		f->center.y += pixel_width(f->zoom_level) * 10;
+	else if (keycode == MK_RIGHT)
+		f->center.x += pixel_width(f->zoom_level) * 10;
+	else if (keycode == MK_DOWN)
+		f->center.y -= pixel_width(f->zoom_level) * 10;
 	return (0);
 }
